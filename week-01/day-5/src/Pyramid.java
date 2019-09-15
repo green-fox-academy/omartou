8c957d15ab697a0864ff72f8e6ac61efcd4f9e90
omartou <omar.toure@yahoo.com>
2019-09-15 23:02:52 +0200

    new exercises added

---
+++ week-01/day-5/src/Pyramid.java
@@ -0,0 +1,17 @@
+public class Pyramid {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        for (int i = 1; i < 4; i++) {
+            for (int j = i; j < 4; j++) {
+                System.out.println(" ");
+                    System.out.println("*");
+
+                }
+
+            }
+
+        }
+    }
+
---
+++ week-01/day-5/src/Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j <= i);
+
+                System.out.print("*");
+        }
+            System.out.println();
+        }
+    }}
+
