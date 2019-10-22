76b609a284da5d304b635c217e093615ec42269f
omartou <omar.toure@yahoo.com>
2019-10-22 14:04:59 +0200

    Greeting exercise added

---
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.springstart.controllers;
+
+public class Greeting {
+
+  private long id = 1;
+  private String content;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+}
